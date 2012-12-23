20e0fb8904ecc22047dd5401717390f80feb0f84
Jorrit Salverda <jorrit.salverda@gmail.com>
2012-12-23 17:51:19 +0100

    added target for sonar analysis
    renamed AnalyzeCode to AnalyzeWithFxCop
    copying dependencies is configurable (default is false)
    fixed bug with azure release having wrong version number in dll's with
    git as cvs and compilecheck skipping compilation

---
+++ Build/Tools/sonar-runner-2.0/bin/sonar-runner.bat
@@ -0,0 +1,78 @@
+@REM Sonar Runner Startup Script for Windows
+@REM
+@REM Required ENV vars:
+@REM   JAVA_HOME - location of a JDK home dir
+@REM
+@REM Optional ENV vars:
+@REM   SONAR_RUNNER_HOME - location of runner's installed home dir
+@REM   SONAR_RUNNER_OPTS - parameters passed to the Java VM when running Sonar
+
+@echo off
+
+
+@REM ==== START VALIDATION ====
+@REM *** JAVA EXEC VALIDATION ***
+if not "%JAVA_HOME%" == "" goto foundJavaHome
+
+for %%i in (java.exe) do set JAVA_EXEC=%%~$PATH:i
+
+if not "%JAVA_EXEC%" == "" (
+  set JAVA_EXEC="%JAVA_EXEC%"
+  goto OkJava
+)
+
+if not "%JAVA_EXEC%" == "" goto OkJava
+
+echo.
+echo ERROR: JAVA_HOME not found in your environment, and no Java 
+echo        executable present in the PATH.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation, or add "java.exe" to the PATH
+echo.
+goto end
+
+:foundJavaHome
+if EXIST "%JAVA_HOME%\bin\java.exe" goto foundJavaExeFromJavaHome
+
+echo.
+echo ERROR: JAVA_HOME exists but does not point to a valid Java home
+echo        folder. No "\bin\java.exe" file can be found there.
+echo.
+goto end
+
+:foundJavaExeFromJavaHome
+set JAVA_EXEC="%JAVA_HOME%\bin\java.exe"
+
+@REM *** SONAR RUNNER HOME VALIDATION ***
+:OkJava
+if NOT "%SONAR_RUNNER_HOME%"=="" goto cleanSonarRunnerHome
+set SONAR_RUNNER_HOME=%~dp0..
+goto run
+
+:cleanSonarRunnerHome
+@REM If the property has a trailing backslash, remove it
+if %SONAR_RUNNER_HOME:~-1%==\ set SONAR_RUNNER_HOME=%SONAR_RUNNER_HOME:~0,-1%
+
+@REM Check if the provided SONAR_RUNNER_HOME is a valid install dir
+IF EXIST "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar" goto run
+
+echo.
+echo ERROR: SONAR_RUNNER_HOME exists but does not point to a valid install
+echo        directory: %SONAR_RUNNER_HOME%
+echo.
+goto end
+
+
+
+@REM ==== START RUN ====
+:run
+echo %SONAR_RUNNER_HOME%
+
+set PROJECT_HOME=%CD%
+
+%JAVA_EXEC% %SONAR_RUNNER_OPTS% -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" "-Dproject.home=%PROJECT_HOME%" org.sonar.runner.Main %*
+
+
+
+@REM ==== END EXECUTION ====
+:end
