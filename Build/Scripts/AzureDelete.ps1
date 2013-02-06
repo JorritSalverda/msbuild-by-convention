468f4c2606a5ddb3db9cfd21ab61c16cfc905568
Jorrit Salverda <jorrit.salverda@gmail.com>
2013-02-06 08:38:49 +0100

    work with newest azure powershell commandlets, sql server data tools
    and data-tier framework (november 2012)

--- Build/Scripts/AzureDelete.ps1
+++ Build/Scripts/AzureDelete.ps1
@@ -1,43 +1,65 @@
 
 # originally posted at https://github.com/JorritSalverda/msbuild-by-convention/
+# based on https://www.windowsazure.com/en-us/develop/net/common-tasks/continuous-delivery/
 
+Param(
+
+	$serviceName = "",
+	$storageAccountName = "",
+	$timeStampFormat = "g",
+
+	$hostedServiceSlot = "Staging",
+
+	$selectedsubscription = "default",
+
+	$certificateFilename = "",
+	$certificatePassword = "",
+	$subscriptionid = ""
+
+)
+
+function DeleteDeployment($slot)
+{
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot "Staging" -ErrorVariable a -ErrorAction silentlycontinue 
+    if ($a[0] -ne $null)
+    {
+        Write-Output "$(Get-Date -f $timeStampFormat) - No deployment is detected. Skipping delete."
+    }
+	
+    #check for existing deployment and then delete
+    if ($deployment.Name -ne $null)
+    {
+		write-progress -id 2 -activity "Deleting Deployment" -Status "In progress"
+		Write-Output "$(Get-Date -f $timeStampFormat) - Deleting Deployment: In progress"
+
+		#WARNING - always deletes with force
+		$removeDeployment = Remove-AzureDeployment -Slot $slot -ServiceName $serviceName -Force
+
+		write-progress -id 2 -activity "Deleting Deployment: Complete" -completed -Status $removeDeployment
+		Write-Output "$(Get-Date -f $timeStampFormat) - Deleting Deployment: Complete"
+	}
+}
+
+#load the management certificate
 $error.clear()
-$subscriptionId = $args[0]
-
-$certificateFilename = $args[1]
-$certificatePassword = $args[2]
+Write-Output "Loading certificate $certificateFilename for subscription $subscriptionid."
 $cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2
 $cert.import($certificateFilename, $certificatePassword, "Exportable,PersistKeySet")
 
-$servicename = $args[3]
-$slot = $args[4]
- 
-if ((Get-PSSnapin | ?{$_.Name -eq "WAPPSCmdlets"}) -eq $null)
-{
-	Add-PSSnapin WAPPSCmdlets
-}
+#load powershell commandlets
+Get-ChildItem 'C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\*.psd1' | ForEach-Object {Import-Module $_}; 
 
-Write-Output "Deleting $servicename, slot $slot."
+Set-AzureSubscription -SubscriptionName $selectedsubscription -SubscriptionId $subscriptionid -Certificate $cert
+Set-AzureSubscription -DefaultSubscription $selectedsubscription
 
-# get 'staging|production' deployment status
-$getHostedServiceStaging = Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot $slot 
- 
-# remove 'staging|production' if exists
-if ($getHostedServiceStaging.Status -ne $null)
-{
-	Write-Output "Instances exists; we're taking them down."
+#set remaining environment variables for Azure cmdlets
+$subscription = Get-AzureSubscription $selectedsubscription
+$subscriptionname = $subscription.subscriptionname
+$subscriptionid = $subscription.subscriptionid
 
-	# set to suspended first
-    $getHostedServiceStaging |
-      Set-DeploymentStatus 'Suspended' |
-      Get-OperationStatus -WaitToComplete
-	  
-	# now remove it
-    $getHostedServiceStaging |
-      Remove-Deployment |
-      Get-OperationStatus -WaitToComplete
-	  
-	Write-Output "Instances are removed."
-}
- 
-if ($error) { exit 888 }
+#main driver - publish & write progress to activity log
+Write-Output "$(Get-Date -f $timeStampFormat) - Azure Cloud Service delete script started."
+
+DeleteDeployment($hostedServiceSlot)
+
+Write-Output "$(Get-Date -f $timeStampFormat) - Azure Cloud Service delete script finished."
--- Build/Scripts/AzureDeploy.ps1
+++ Build/Scripts/AzureDeploy.ps1
@@ -1,184 +1,257 @@
 
 # originally posted at https://github.com/JorritSalverda/msbuild-by-convention/
+# based on https://www.windowsazure.com/en-us/develop/net/common-tasks/continuous-delivery/
 
+Param(
+
+	$serviceName = "",
+	$storageAccountName = "",
+	$packageLocation = "",
+	$cloudConfigLocation = "",
+	$deploymentLabel = "ContinuousDeploy to $servicename",
+	$timeStampFormat = "s",
+
+	$swapAfterDeploy = "True",
+	$deleteStagingAfterSwap = "True",
+	$enableDeploymentUpgrade = "True",
+
+	$selectedsubscription = "default",
+
+	$certificateFilename = "",
+	$certificatePassword = "",
+	$subscriptionid = ""
+
+)
+
+function PublishAndSwap()
+{
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot "Staging" -ErrorVariable a -ErrorAction silentlycontinue 
+    if ($a[0] -ne $null)
+    {
+        Write-Output "$(Get-Date -f $timeStampFormat) - No deployment is detected. Creating a new deployment. "
+    }
+	
+    #check for existing deployment and then either upgrade, delete + deploy, or cancel according to $alwaysDeleteExistingDeployments and $enableDeploymentUpgrade boolean variables
+    if ($deployment.Name -ne $null)
+    {
+		Write-Output "$(Get-Date -f $timeStampFormat) - Deployment exists in $servicename.  Deleting deployment."
+		DeleteDeployment("Staging")
+    }
+	
+	CreateNewDeployment("Staging")
+	
+	if ($swapAfterDeploy -eq "True")
+	{
+		SpinUpDeployment("Staging")
+		SwapDeployments
+		SpinUpDeployment("Production")
+		if ($deleteStagingAfterSwap -eq "True") 
+		{
+			DeleteDeployment("Staging")
+		}
+	}
+}
+
+function PublishAndUpgrade()
+{
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot "Production" -ErrorVariable a -ErrorAction silentlycontinue 
+    if ($a[0] -ne $null)
+    {
+        Write-Output "$(Get-Date -f $timeStampFormat) - No deployment is detected. Creating a new deployment. "
+    }
+    #check for existing deployment and then either upgrade, delete + deploy, or cancel according to $alwaysDeleteExistingDeployments and $enableDeploymentUpgrade boolean variables
+    if ($deployment.Name -ne $null)
+    {
+		UpgradeDeployment("Production")		
+    } 
+	else 
+	{
+		CreateNewDeployment("Production")	
+    }
+	SpinUpDeployment("Production")
+}
+
+function CreateNewDeployment($slot)
+{
+    write-progress -id 3 -activity "Creating New Deployment" -Status "In progress"
+    Write-Output "$(Get-Date -f $timeStampFormat) - Creating New Deployment: In progress"
+	
+    $opstat = New-AzureDeployment -Slot $slot -Package $packageLocation -Configuration $cloudConfigLocation -label $deploymentLabel -ServiceName $serviceName
+
+    $completeDeployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $completeDeploymentID = $completeDeployment.deploymentid
+
+    write-progress -id 3 -activity "Creating New Deployment" -completed -Status "Complete"
+    Write-Output "$(Get-Date -f $timeStampFormat) - Creating New Deployment: Complete, Deployment ID: $completeDeploymentID"
+
+    StartInstances($slot)
+}
+
+function UpgradeDeployment($slot)
+{
+    write-progress -id 3 -activity "Upgrading Deployment" -Status "In progress"
+    Write-Output "$(Get-Date -f $timeStampFormat) - Upgrading Deployment: In progress"
+
+    # perform Update-Deployment
+    $setdeployment = Set-AzureDeployment -Upgrade -Slot $slot -Package $packageLocation -Configuration $cloudConfigLocation -label $deploymentLabel -ServiceName $serviceName -Force
+
+    $completeDeployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $completeDeploymentID = $completeDeployment.deploymentid
+
+    write-progress -id 3 -activity "Upgrading Deployment" -completed -Status "Complete"
+    Write-Output "$(Get-Date -f $timeStampFormat) - Upgrading Deployment: Complete, Deployment ID: $completeDeploymentID"
+}
+
+function DeleteDeployment($slot)
+{
+    write-progress -id 2 -activity "Deleting Deployment" -Status "In progress"
+    Write-Output "$(Get-Date -f $timeStampFormat) - Deleting Deployment: In progress"
+
+    #WARNING - always deletes with force
+    $removeDeployment = Remove-AzureDeployment -Slot $slot -ServiceName $serviceName -Force
+
+    write-progress -id 2 -activity "Deleting Deployment: Complete" -completed -Status $removeDeployment
+    Write-Output "$(Get-Date -f $timeStampFormat) - Deleting Deployment: Complete"
+}
+
+function StartInstances($slot)
+{
+    write-progress -id 4 -activity "Starting Instances" -status "In progress"
+    Write-Output "$(Get-Date -f $timeStampFormat) - Starting Instances: In progress"
+
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $runstatus = $deployment.Status
+
+    if ($runstatus -ne 'Running') 
+    {
+        $run = Set-AzureDeployment -Slot $slot -ServiceName $serviceName -Status Running
+    }
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $oldStatusStr = @("") * $deployment.RoleInstanceList.Count
+
+    while (-not(AllInstancesRunning($deployment.RoleInstanceList)))
+    {
+        $i = 1
+        foreach ($roleInstance in $deployment.RoleInstanceList)
+        {
+            $instanceName = $roleInstance.InstanceName
+            $instanceStatus = $roleInstance.InstanceStatus
+
+            if ($oldStatusStr[$i - 1] -ne $roleInstance.InstanceStatus)
+            {
+                $oldStatusStr[$i - 1] = $roleInstance.InstanceStatus
+                Write-Output "$(Get-Date -f $timeStampFormat) - Starting Instance '$instanceName': $instanceStatus"
+            }
+
+            write-progress -id (4 + $i) -activity "Starting Instance '$instanceName'" -status "$instanceStatus"
+            $i = $i + 1
+        }
+
+        sleep -Seconds 1
+
+        $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    }
+
+    $i = 1
+    foreach ($roleInstance in $deployment.RoleInstanceList)
+    {
+        $instanceName = $roleInstance.InstanceName
+        $instanceStatus = $roleInstance.InstanceStatus
+
+        if ($oldStatusStr[$i - 1] -ne $roleInstance.InstanceStatus)
+        {
+            $oldStatusStr[$i - 1] = $roleInstance.InstanceStatus
+            Write-Output "$(Get-Date -f $timeStampFormat) - Starting Instance '$instanceName': $instanceStatus"
+        }
+
+        $i = $i + 1
+    }
+
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $opstat = $deployment.Status 
+
+    write-progress -id 4 -activity "Starting Instances" -completed -status $opstat
+    Write-Output "$(Get-Date -f $timeStampFormat) - Starting Instances: $opstat"
+}
+
+function AllInstancesRunning($roleInstanceList)
+{
+    foreach ($roleInstance in $roleInstanceList)
+    {
+        if ($roleInstance.InstanceStatus -ne "ReadyRole")
+        {
+            return $false
+        }
+    }
+
+    return $true
+}
+
+function SpinUpDeployment($slot)
+{
+	$deployment = Get-AzureDeployment -slot $slot -serviceName $servicename
+	$deploymentUrl = $deployment.Url
+	$webclient = new-object net.WebClient
+
+    Write-Output "$(Get-Date -f $timeStampFormat) - Spinning up deployment slot '$slot' at url '$deploymentUrl': In progress"
+
+	Try
+ 	{
+		[void]$webclient.downloadData($deploymentUrl)
+ 	}
+	Catch [system.exception]
+	{
+	}
+
+    Write-Output "$(Get-Date -f $timeStampFormat) - Spinning up deployment slot '$slot' at url '$deploymentUrl': Done"
+}
+
+function SwapDeployments()
+{
+    write-progress -id 5 -activity "Swapping slots" -status "In progress"
+    Write-Output "$(Get-Date -f $timeStampFormat) - Swapping slots: In progress"
+
+	$move = Move-AzureDeployment -ServiceName $serviceName
+	
+    write-progress -id 5 -activity "Swapping slots" -completed -status $move
+    Write-Output "$(Get-Date -f $timeStampFormat) - Swapping slots: $move"	
+}
+
+#load the management certificate
 $error.clear()
-$subscriptionId = $args[0]
-
-$certificateFilename = $args[1]
-$certificatePassword = $args[2]
+Write-Output "Loading certificate $certificateFilename for subscription $subscriptionid."
 $cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2
 $cert.import($certificateFilename, $certificatePassword, "Exportable,PersistKeySet")
 
-$buildPath = $args[3]
-$packagename = $args[4]
-$serviceconfig = $args[5]
-$servicename = $args[6]
-$storageAccount = $args[7]
-$storageAccountKey = $args[8]
-$buildVersion = $args[9]
-$swapToProductionAfterDeploy = $args[10]
-$removeStagingAfterSwap = $args[11]
-$disallowMultipleActiveInstances = $args[12]
+#load powershell commandlets
+Get-ChildItem 'C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\*.psd1' | ForEach-Object {Import-Module $_}; 
 
-$package = join-path $buildPath $packageName
-$config = join-path $buildPath $serviceconfig
-$a = Get-Date
-$buildLabel = $buildVersion + " (" + $a.ToShortDateString() + "-" + $a.ToShortTimeString() + ")"
- 
-if ((Get-PSSnapin | ?{$_.Name -eq "WAPPSCmdlets"}) -eq $null)
+Write-Output "Setting storage account $storageAccountName."
+Set-AzureSubscription -CurrentStorageAccount $storageAccountName -SubscriptionName $selectedsubscription -SubscriptionId $subscriptionid -Certificate $cert
+Set-AzureSubscription -DefaultSubscription $selectedsubscription
+
+#set remaining environment variables for Azure cmdlets
+$subscription = Get-AzureSubscription $selectedsubscription
+$subscriptionname = $subscription.subscriptionname
+$subscriptionid = $subscription.subscriptionid
+
+#main driver - publish & write progress to activity log
+Write-Output "$(Get-Date -f $timeStampFormat) - Azure Cloud Service deploy script started."
+if ($enableDeploymentUpgrade -eq "True")
 {
-	Add-PSSnapin WAPPSCmdlets
-}
+	Write-Output "$(Get-Date -f $timeStampFormat) - Preparing in-place upgrade of $deploymentLabel for $subscriptionname with Subscription ID $subscriptionid."
 
-Write-Output "Deploying $packagename version $buildVersion to $servicename."
-
-# get 'staging' deployment status
-$getHostedServiceStaging = Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot 'Staging' 
- 
-# remove old 'staging' deploy if exists
-if ($getHostedServiceStaging.Status -ne $null)
-{
-	Write-Output "Staging instances already exists; we're taking them down."
-
-	# set to suspended first
-    $getHostedServiceStaging |
-      Set-DeploymentStatus 'Suspended' |
-      Get-OperationStatus -WaitToComplete
-	  
-	# now remove it
-    $getHostedServiceStaging |
-      Remove-Deployment |
-      Get-OperationStatus -WaitToComplete
-	  
-	Write-Output "Staging instances are removed."
-}
-
-Write-Output "Deploying new package."
-
-# deploy the package to 'staging'
-Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | 
-    New-Deployment 'Staging' -package $package -configuration $config -label $buildLabel -serviceName $servicename -StorageServiceName $storageAccount |
-    Get-OperationStatus -WaitToComplete
-
-Write-Output "Done deploying new package."	
-	
-if ($disallowMultipleActiveInstances -ne "True")
-{
-	Write-Output "Spinning up staging instances."
-		
-	# spin the deployment up to 'running'
-	Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot 'Staging' |
-		Set-DeploymentStatus 'Running' |
-		Get-OperationStatus -WaitToComplete
-	 
-	# the completion of previous action doesn't necessarily mean it's really ready, so check the status until it is 'Ready' and the name until it matches the one we used for our deployment
-	$ready = $False
-	while(!$ready)
-	{
-		$d = Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot 'Staging' 
-		$ready = ($d.RoleInstanceList[0].InstanceStatus -eq "Ready") -and ($d.Label -eq $buildLabel)
-	} 
-
-	Write-Output "Done spinning up staging instances."
-	
-	$d = Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot 'Staging' 
-	$stagingurl = $d.Url
-	
-	Write-Output "Starting staging website at url $stagingurl"
-	$webclient = new-object net.WebClient
-	Try
-	{
-		[void]$webclient.downloadData($stagingurl)
-	}
-	Catch [system.exception]
-	{
-		$error.clear()
-	}
+	PublishAndUpgrade
 }
 else
 {
-	Write-Output "Keeping staging suspended, because only 1 active instance is allowed."
-}
- 
-# if $swapToProductionAfterDeploy argument equals 'True' we want to deploy all the way to production, so we swap instances; for the real production environment we would probably do this by hand
-if ($swapToProductionAfterDeploy -eq "True")
-{
-	if ($disallowMultipleActiveInstances -eq "True")
-	{	
-		# get 'production' deployment status 
-		$getHostedServiceStaging = Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot 'Production' 
-		 
-		# suspend 'staging' deploy if exists, which only is the case if we had a former 'Production' instance
-		if ($getHostedServiceStaging.Status -ne $null)
-		{
-			Write-Output "Suspending production first, so only 1 instance is active."
-		
-			# set to suspended first
-			$getHostedServiceStaging |
-			  Set-DeploymentStatus 'Suspended' |
-			  Get-OperationStatus -WaitToComplete
-		}	
-	}
+	Write-Output "$(Get-Date -f $timeStampFormat) - Preparing deployment of $deploymentLabel for $subscriptionname with Subscription ID $subscriptionid."
 
-	Write-Output "Start swapping staging and production instances."
-
-	# swap 'Staging' and 'Production'
-	Get-Deployment staging -subscriptionId $subscriptionId -certificate $cert -serviceName $servicename | 
-		Move-Deployment |
-		Get-OperationStatus -WaitToComplete
-
-	Write-Output "Done swapping staging and production instances."
-	
-	Write-Output "Spinning up production instances."
-		
-	# spin production up to 'running' if that wasn't already the case
-	Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot 'Production' |
-		Set-DeploymentStatus 'Running' |
-		Get-OperationStatus -WaitToComplete
-		
-	# the completion of previous action doesn't necessarily mean it's really ready, so check the status until it is 'Ready' and the name until it matches the one we used for our deployment
-	$ready = $False
-	while(!$ready)
-	{
-		$d = Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId |
-				Get-Deployment -Slot 'Production'
-		$ready = ($d.RoleInstanceList[0].InstanceStatus -eq "Ready") -and ($d.Label -eq $buildLabel)
-	}		
-	
-	Write-Output "Done spinning up production instances."
-	
-	# get 'staging' deployment status 
-	$getHostedServiceStaging = Get-HostedService $servicename -Certificate $cert -SubscriptionId $subscriptionId | Get-Deployment -Slot 'Staging' 
-	 
-	# remove old 'staging' deploy if exists, which only is the case if we had a former 'Production' instance
-	if ($getHostedServiceStaging.Status -ne $null)
-	{
-		Write-Output "Suspending staging instances."
-		
-		# set to suspended first
-		$getHostedServiceStaging |
-		  Set-DeploymentStatus 'Suspended' |
-		  Get-OperationStatus -WaitToComplete
-
-		Write-Output "Done suspending staging instances."
-		  
-		# if $removeStagingAfterSwap argument equals 'True' we remove staging; we only do this if $swapToProductionAfterDeploy equals 'True' as well
-		if ($removeStagingAfterSwap -eq "True")
-		{
-			Write-Output "Removing staging instances."
-		  
-			# now remove it
-			$getHostedServiceStaging |
-			  Remove-Deployment |
-			  Get-OperationStatus -WaitToComplete
-			  
-			Write-Output "Done removing staging instances."
-		}
-	}	
+	PublishAndSwap
 }
 
-#remove temporary blob storage containers to avoid unnecessary usage and costs
-Get-Container -StorageAccountName $storageAccount -StorageAccountKey $storageAccountKey -Filter 'mydeployments' | Clear-Container
-Get-Container -StorageAccountName $storageAccount -StorageAccountKey $storageAccountKey -Filter 'wad-control-container' | Clear-Container
- 
-if ($error) { exit 888 }
+$deployment = Get-AzureDeployment -slot "Production" -serviceName $servicename
+$deploymentUrl = $deployment.Url
+
+Write-Output "$(Get-Date -f $timeStampFormat) - Created Cloud Service with URL $deploymentUrl."
+Write-Output "$(Get-Date -f $timeStampFormat) - Azure Cloud Service deploy script finished."
